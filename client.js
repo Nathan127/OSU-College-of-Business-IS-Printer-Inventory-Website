b1a792183f5f6fe5bdcf10376d99ebc30c8e3610
Carter Olsen <olsencar@oregonstate.edu>
2017-11-16 19:45:26 -0800

    Fixed an issue where the quantity filter would update

--- client.js
+++ client.js
@@ -25,14 +25,7 @@ function contentClick (event) {
     }
     else if (target.id == 'filter-update-button') {
         filter(target);
-    }
-    console.log(document.getElementById('sort-type').value);
-    // else if (target.id == 'submitSort') {
-    //     console.log(document.getElementById('sort-type').value);
-    //     sort(event.currentTarget);
-    // }// add a submit button to the sort
-
-    
+    }   
 }
 
 function changeQuantity(target) {
@@ -54,7 +47,6 @@ function changeQuantity(target) {
 
 function editNotes (target) {
     var text = target.parentNode.previousElementSibling.textContent;
-
 }
 
 function filter (target) {
@@ -63,7 +55,7 @@ function filter (target) {
     var td;
     var quantity;
     var foundMatch = false;
-    var tr = table.getElementsByTagName('tr');
+    var tr = table.getElementsByTagName('TR');
     var numRows = tr.length;
     var color;
 
@@ -75,14 +67,20 @@ function filter (target) {
         document.getElementById('filter-color').value
     );
 
+    var columns = {};
+
+    td = tr[1].getElementsByClassName('TD');
+    for (i = 0; i < td.length; i++) {
+        columns[td[i].textContent] = i;
+    }
+    // Show each row to start
     for (i = start; i < numRows - 1; i++) {
         tr[i].style.display = 'table-row';
     }
 
-    console.log(filter);
     var key = new RegExp(filter.searchKey, 'i');
 
-    // Search key
+    // Search filter
     for (i = start; i < numRows - 1; i++) {    
         foundMatch = false;
         td = tr[i].getElementsByTagName('TD');
@@ -111,21 +109,19 @@ function filter (target) {
             quantity[j].style.display = 'block';
             quantity[j].nextElementSibling.style.display = 'block';
             td[5].children[j].style.display = 'block';
-
+            
             if (filter.maxQuantity === 0) {
                 filter.maxQuantity = 9999999;
             }
-
+            console.log("== Before", quantity[j].style.display + " " + i);
             if (Number(quantity[j].textContent) < filter.minQuantity || Number(quantity[j].textContent) > filter.maxQuantity) {
-                console.log(Number(quantity[j].textContent));
-                console.log(filter.minQuantity);
                 // set the display of all rows not meeting quantity standards to 'none'
                 td[2].children[j].style.display = 'none';
                 td[3].children[j].style.display = 'none';
                 quantity[j].style.display = 'none';
                 quantity[j].nextElementSibling.style.display = 'none';
                 td[5].children[j].style.display = 'none';
-
+                console.log(quantity[j].style.display + '  ' + i);
                 // The commented out code below is in case we decide that we would rather change the color of the match
                 //      instead of the display value 
 
@@ -158,157 +154,44 @@ function filter (target) {
 
     // Color filter
     // For HTML add an attribute that will give the color
-    for (i = start; i < numRows - 1; i++) {
+    if (filter.color != '') {
+        for (i = start; i < numRows - 1; i++) {
+            
+            td = tr[i].getElementsByTagName('TD');
+            color = td[3].getElementsByClassName('color');
+    
+            for (j = 0; j < color.length; j++) {
+                // reset all rows back to normal
+                td[2].children[j].style.display = 'block';
+                color[j].style.display = 'block';
+                td[4].children[j].style.display = 'block';
+                td[5].children[j].style.display = 'block';
+                
+                if (filter.color != '') {
+                    if (color[j].textContent.search(filter.color) === -1) {
+                        // set the display of all rows not meeting quantity standards to 'none'
+                        td[2].children[j].style.display = 'none';
+                        color[j].style.display = 'none';
+                        td[4].children[j].style.display = 'none';
+                        td[5].children[j].style.display = 'none';
         
-        td = tr[i].getElementsByTagName('TD');
-        color = td[3].getElementsByClassName('color');
-
-        for (j = 0; j < color.length; j++) {
-            // reset all rows back to normal
-            td[2].children[j].style.display = 'block';
-            color[j].style.display = 'block';
-            td[4].children[j].style.display = 'block';
-            td[5].children[j].style.display = 'block';
-            console.log(color[j].textContent);
-            if (filter.color != '') {
-                if (color[j].textContent.search(filter.color) === -1) {
-                    // set the display of all rows not meeting quantity standards to 'none'
-                    td[2].children[j].style.display = 'none';
-                    color[j].style.display = 'none';
-                    td[4].children[j].style.display = 'none';
-                    td[5].children[j].style.display = 'none';
-    
-                    // The commented out code below is in case we decide that we would rather change the color of the match
-                    //      instead of the display value 
-    
-                    // if (quantity[j].getAttribute('color').toLowerCase() == 'black') {
-                    //     quantity[j].style.color = 'white';
-                    // }
-                    // quantity[j].style.backgroundColor = quantity[j].getAttribute('color');
+                        // The commented out code below is in case we decide that we would rather change the color of the match
+                        //      instead of the display value 
+        
+                        // if (quantity[j].getAttribute('color').toLowerCase() == 'black') {
+                        //     quantity[j].style.color = 'white';
+                        // }
+                        // quantity[j].style.backgroundColor = quantity[j].getAttribute('color');
+                    }
+                
                 }
-            
             }
+    
+                
         }
-
-            
     }
+    
 
     
 }
 content.addEventListener('click', contentClick);
-
-
-
-
-
-
-
-
-
-
-
-// function sortAlpha (table) {
-    
-//     // var numOfPrinters = null;
-//     // var start = 2;
-//     // var j = start;
-//     // var x;
-//     for (var i = 0; i < table.rows.length; i++) {
-//         console.log(table.rows[i]);
-//     }
-//     table.rows = selectionSort(table.rows)
-//     for (var i = 0; i < table.rows.length; i++) {
-//         console.log(table.rows[i]);
-//     }
-//     return table.rows;
-    
-//     // for (var x = 0; x < i; x++) {
-//     //     rows.shift();
-//     // }
-// }
-// function selectionSort(items){
-    
-//         var len = items.length,
-//             min;
-    
-//         for (i=2; i < len; i++){
-    
-//             //set minimum to this position
-//             min = i;
-    
-//             //check the rest of the array to see if anything is smaller
-//             for (j=i+1; j < len; j++){
-//                 if (items[j].cells[0].textContent < items[min].cells[0].textContent){
-//                     min = j;
-//                 }
-//             }
-    
-//             //if the minimum isn't in the position, swap it
-//             if (i != min){
-//                 swap(items, i, min);
-//             }
-//         }
-    
-//         return items;
-// }
-// function swap(items, firstIndex, secondIndex){
-//     var temp = items[firstIndex];
-//     items[firstIndex] = items[secondIndex];
-//     items[secondIndex] = temp;
-// }
-// 
-/*  while (i < rows.length) {
-        for (var j = i + 1; j < rows.length; j++) {
-            if (rows[j].cells[0].textContent < rows[i].cells[0].textContent) {
-                tempRow = rows[i];
-                rows[i] = rows[j];
-                rows[j] = tempRow;
-                j++;
-                i++;
-            }
-        }
-        i++;
-    }
-    
-
-    var tempRow = rows[j];
-    var xRowspan, jRowspan;
-
-    while (j < rows.length) {
-        x = j + jRowspan
-        xRowspan = rows[x].cells[0].rowSpan;
-        jRowspan = rows[j].cells[0].rowSpan;
-
-        while (x >= j) {
-            if (rows[x].cells[0].textContent < rows[j].cells[0].textContent) {
-                if (xRowspan > jRowspan) {
-                    var rowSpanGT, rowSpanLT;
-                    if (xRowspan > jRowspan) {
-                        rowSpanGT = xRowspan;
-                        rowSpanLT = jRowspan;
-                    }  
-                    else  {
-                        rowSpanGT = jRowspan;
-                        rowSpanLT = xRowspan;
-                    }
-                        
-                   
-                    for (var k = 0; k <= rowSpanGT; k++) {
-                        if (j + k > rowSpanLT) {
-                            for (var z = x + rowSpanGT; z > x; z--) {
-                                tempRow = rows[x + rowSpanGT - z];
-                                rows[x + rowSpanGT - z] = rows[rowSpanGT];
-                                rows[rowSpanGT] = tempRow;
-                            }
-                        }
-                        else {
-                            tempRow = rows[j + k];
-                            rows[j + k] = rows[x + k];
-                            rows[x + k] = tempRow;
-                        }
-                        
-                    }
-                    }
-                }
-            }
-            */
