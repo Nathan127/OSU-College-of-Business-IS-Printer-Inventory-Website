c10f24620cd2c324ce58d7160569677c48155e35
Carter Olsen <olsencar@oregonstate.edu>
2017-11-17 22:25:14 -0800

    HTML: Fixed an issue where a minus button was adding to quantity.
    JS: Completed functionality of the filter.

--- client.js
+++ client.js
@@ -5,16 +5,13 @@ var cancel = document.getElementById('modal-cancel');
 var modal = document.getElementById('sell-something-modal');
 var backdropModal = document.getElementById('modal-backdrop');
 var post = document.getElementById('modal-accept');
-var reset = document.getElementById('reset-button');
 var open = document.getElementById('add-new-item');
-var defaultSort = null;
 
 open.addEventListener("click", openmodal);
 close.addEventListener("click", closemodal);
 cancel.addEventListener("click", closemodal);
 document.addEventListener("click", windowCloseModal);
 post.addEventListener("click", submit);
-reset.addEventListener("click", resetTable);
 
 function resetTable(target) {
     document.getElementById('filter-search').value = '';
@@ -213,8 +210,7 @@ function Filter(searchKey, minQuantity, maxQuantity, brand, color) {
 
 function contentClick(event) {
     var target = event.target;
-
-    if (target.id === 'change') {
+    if (target.className === 'change') {
         changeQuantity(target);
     }
     else if (target.id === 'edit') {
@@ -229,13 +225,11 @@ function contentClick(event) {
 }
 
 function changeQuantity(target) {
-    var text = target.parentNode.previousElementSibling.textContent;
-    var quantity = Number(text);
-
-    if (target.textContent == '(+1)') { //figure out why this isn't working...
+    var quantity = Number(target.parentNode.previousElementSibling.textContent);
+    if (target.value === 'add') { //figure out why this isn't working...
         quantity += 1;
     }
-    else if (target.textContent == '(-1)' && quantity <= 0) {
+    else if (target.value === 'minus' && quantity <= 0) {
         alert("Toner quantity cannot be lower than 0");
     }
     else {
@@ -258,6 +252,7 @@ function filter(target) {
     var numRows = tr.length;
     var color;
     var columns = {};
+    var matchesFound;
 
     var filter = new Filter(
         document.getElementById('filter-search').value,
@@ -282,7 +277,7 @@ function filter(target) {
     var key = new RegExp(filter.searchKey, 'i');
 
     // Search filter
-    for (i = start; i < numRows - 1; i++) {
+    for (i = start; i < numRows; i++) {
         foundMatch = false;
         td = tr[i].getElementsByTagName('TD');
         for (j = 0; j < td.length; j++) {
@@ -298,13 +293,14 @@ function filter(target) {
     }
 
     // min Quantity and max quantity filter
-    for (i = start; i < numRows - 1; i++) {
+    for (i = start; i < numRows; i++) {
 
         td = tr[i].getElementsByTagName('TD');
         quantity = td[columns['Quantity']].getElementsByClassName('quantity');
+        matchesFound = 0;
 
         for (j = 0; j < quantity.length; j++) {
-            // reset all rows back to normal
+            // reset all columns and rows back to normal
             td[columns['# Code']].children[j].style.display = 'block';
             td[columns['Color']].children[j].style.display = 'block';
             quantity[j].style.display = 'block';
@@ -322,13 +318,17 @@ function filter(target) {
                 quantity[j].style.display = 'none';
                 quantity[j].nextElementSibling.style.display = 'none';
                 td[columns['Last-Updated']].children[j].style.display = 'none';
+                matchesFound++;
+            }
+            if (matchesFound === quantity.length) {
+                tr[i].style.display = 'none';
             }
         }
     }
 
     // Brand filter
     if (filter.brand.length > 0) {
-        for (i = start; i < numRows - 1; i++) {
+        for (i = start; i < numRows; i++) {
             td = tr[i].getElementsByTagName('TD');
             var brandName = td[0].textContent.toLowerCase();
             foundMatch = false;
@@ -348,31 +348,36 @@ function filter(target) {
     // Color filter
     // For HTML add an attribute that will give the color
     if (filter.color != '') {
-        for (i = start; i < numRows - 1; i++) {
+        for (i = start; i < numRows; i++) {
 
             td = tr[i].getElementsByTagName('TD');
-            color = td[3].getElementsByClassName('color');
+            color = td[columns.Color].getElementsByClassName('color');
+            quantity = td[columns.Quantity].getElementsByClassName('quantity');
+            matchesFound = 0;
 
             for (j = 0; j < color.length; j++) {
                 // reset all rows back to normal
-                td[2].children[j].style.display = 'block';
-                color[j].style.display = 'block';
-                td[4].children[j].style.display = 'block';
-                td[5].children[j].style.display = 'block';
+                td[columns['# Code']].children[j].style.display = 'block';
+                color[j].style.display = 'block';           
+                quantity[j].style.display = 'block';
+                quantity[j].nextElementSibling.style.display = 'block';
+                td[columns['Last-Updated']].children[j].style.display = 'block';
 
-                if (filter.color != '') {
-                    if (color[j].textContent.search(filter.color) === -1) {
-                        // set the display of all rows not meeting quantity standards to 'none'
-                        td[2].children[j].style.display = 'none';
-                        color[j].style.display = 'none';
-                        td[4].children[j].style.display = 'none';
-                        td[5].children[j].style.display = 'none';
-                    }
+                
+                if (color[j].textContent.search(filter.color) === -1) {
+                    // set the display of all rows not meeting quantity standards to 'none'
+                    td[columns['# Code']].children[j].style.display = 'none';
+                    color[j].style.display = 'none';                   
+                    quantity[j].style.display = 'none';
+                    quantity[j].nextElementSibling.style.display = 'none';
+                    td[columns['Last-Updated']].children[j].style.display = 'none';
+                    matchesFound++;
+                }
 
+                if (matchesFound === color.length) {
+                    tr[i].style.display = 'none';
                 }
             }
-
-
         }
     }
 }
